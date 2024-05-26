c40233d2b9b930e69737b14a8eb5b29026f50df9
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-26 15:21:29 -0700

    Minor changes to allow script to run anywhere

--- src/vscode_cleanup/__main__.py
+++ src/vscode_cleanup/__main__.py
@@ -3,6 +3,14 @@ import os  # Importing the os library for interacting with the operating system
 import semver  # Importing the semver library for parsing semantic version strings
 
 
+def get_extension_path(search_path):
+    if os.path.isdir(os.path.join(search_path, 'extensions')):
+        extensions_path = os.path.join(search_path, 'extensions')        
+    else:
+        extensions_path = search_path
+    return os.path.abspath(extensions_path)
+
+
 def get_extension_data(extensions_path):
     """
     Function to retrieve extension data from a given directory.
@@ -14,6 +22,7 @@ def get_extension_data(extensions_path):
         dict: A dictionary containing extension names as keys and lists of semantic versions as values.
     """
     extension_data = {}
+        
     for extension in os.listdir(extensions_path):  # Iterate through each file/directory in the extensions directory
         extension_path = os.path.join(extensions_path, extension)  # Create full path to the extension
         if os.path.isdir(extension_path):  # Check if the current item is a directory
@@ -21,8 +30,8 @@ def get_extension_data(extensions_path):
                 name, version = extension.rsplit("-",
                                                  1)  # Split the filename at the last "-" to separate name and version
                 version = semver.VersionInfo.parse(version)  # Parse the version string as semantic version
-                extension_data.setdefault(name, []).append(
-                    version)  # Store the version in the dictionary under the extension name
+                # Store the version in the dictionary under the extension name
+                extension_data.setdefault(name, []).append(version)  
             except ValueError as e:
                 print(f"Error parsing version for {extension}: {e}. Skipping.")
             except AttributeError:
@@ -84,12 +93,13 @@ def remove_duplicates(duplicate_extensions, latest_versions, extensions_path):
     """
     action_for_duplicates = input("Remove old duplicates? (yes/no): ").lower() == "yes"
     if action_for_duplicates:
-        os.makedirs("old_versions", exist_ok=True)
+        deprecated_path = os.path.join(extensions_path, ".duplicates")       
+        os.makedirs(deprecated_path, exist_ok=True)
         for name, versions in duplicate_extensions.items():
             for version in versions:
                 if version != latest_versions[name]:
                     old_extension_path = os.path.join(extensions_path, f"{name}-{version}")
-                    new_extension_path = os.path.join("old_versions", f"{name}-{version}")
+                    new_extension_path = os.path.join(deprecated_path, f"{name}-{version}")
                     os.renames(old_extension_path, new_extension_path)
                     print(f"Moved {old_extension_path} to {new_extension_path}")
     else:
@@ -105,7 +115,7 @@ def main():
     parser.add_argument("extensions_path", type=str, help="Path to the directory containing extensions")
     args = parser.parse_args()
 
-    extensions_path = args.extensions_path  # Using the argument as the path
+    extensions_path = get_extension_path(args.extensions_path)
     extension_data = get_extension_data(extensions_path)
     duplicate_extensions = find_duplicates(extension_data)
     latest_versions = get_latest_versions(duplicate_extensions)
